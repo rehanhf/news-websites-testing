67d9e47ae595168c290792cd46eeaf0875c6e046
reei <hafizhnabil@gmail.com>
2025-11-21 18:10:18 +0700

    api delete

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,12 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKLOe1hp_RmoAQmPU_l4C0NqG_XSXsbSQ",
-  authDomain: "database-test-b50aa.firebaseapp.com",
-  projectId: "database-test-b50aa",
-  storageBucket: "database-test-b50aa.firebasestorage.app",
-  messagingSenderId: "864335423664",
-  appId: "1:864335423664:web:d061ee2ca555e58fa3b375",
-  measurementId: "G-L9QFTMCG31"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase only if it hasn't been initialized yet
